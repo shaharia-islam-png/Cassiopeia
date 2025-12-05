c1e71c7e4448cc57ac4c20b93bf510203c198a41
SHAHARIA ISLAM <islamshaharia2006@gmail.com>
2025-12-05 23:09:09 +0600

    Fix formatting in vite.config.js plugins array

--- todo-lists/vite.config.js
+++ todo-lists/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  plugins: [ 
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
